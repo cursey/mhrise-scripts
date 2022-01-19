3a7de6506a878bc597a224398da4c3d9e092cbd5
cursey <cursey@users.noreply.github.com>
2022-01-19 02:00:40 -0800

    Update weapon_stay_big.lua
    * Refactored to be much lighter weight (0 impact on FPS)
    * Now affects all players in a lobby
    * Does not make kinsect big

--- weapon_stay_big.lua
+++ weapon_stay_big.lua
@@ -1,24 +1,9 @@
 log.info("[weapon_stay_big.lua] loaded")
 
-local identity3f = Vector3f.new(1, 1, 1)
+local typedef = sdk.find_type_definition("snow.player.PlayerWeaponCtrl")
+local start_method = typedef:get_method("start")
 
-re.on_application_entry("PrepareRendering", function()
-    local playman = sdk.get_managed_singleton("snow.player.PlayerManager")
-    if not playman then return end
-    local me = playman:call("findMasterPlayer")
-    if not me then return end
-
-    -- Scale the main weapon back to 100%
-    local weapon = me:get_field("_WeaponMain")
-    if not weapon then return end
-    local transform = weapon:call("get_Transform")
-    if not transform then return end
-    transform:call("set_LocalScale", identity3f)
-
-    -- Scale the sub weapon (LS scabbard for instance) back to 100%
-    weapon = me:get_field("_WeaponSub")
-    if not weapon then return end
-    transform = weapon:call("get_Transform")
-    if not transform then return end
-    transform:call("set_LocalScale", identity3f)
-end)
+sdk.hook(start_method, function(args)
+    local weapctrl = sdk.to_managed_object(args[2])
+    weapctrl:set_field("_bodyConstScale", 1.0)
+end, function(retval) end)
